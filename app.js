474addc2b3fd05765a516139ed449fbbd5d66e60
pipi <piaqasalas@gmail.com>
2026-02-09 16:39:52 -0300

    app

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 import "./travel-tabs.js";
 import "./travel-ui.js";
+
 /* MODULOS */
 import "./clients.js";
 import "./services.js";
 import "./pdf.js";
 
-/*************************************************
- * BOOTSTRAP APP
- *************************************************/
 document.addEventListener("DOMContentLoaded", initApp);
 
-
-
+function initApp() {
+  console.log("APP cargada");
+}
