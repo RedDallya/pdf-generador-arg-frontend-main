72cfd772617c3a891a48e11657e6fb50639df888
pipi <piaqasalas@gmail.com>
2026-02-09 14:30:22 -0300

    app

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 import { appState } from "./state.js";
 import { createEmptyTravel } from "./travels.js";
-import "./clients.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("APP cargada");
-});
-
-// Inicializadores de módulos
+/* MODULOS */
 import "./clients.js";
 import "./services.js";
 import "./pdf.js";
@@ -16,12 +11,30 @@ import "./pdf.js";
  *************************************************/
 document.addEventListener("DOMContentLoaded", initApp);
 
+function initApp() {
+
+  console.log("APP cargada");
+
+  ensureDefaultTravel();
+
+}
+
 
 /*************************************************
  * DEFAULT TRAVEL
  *************************************************/
 function ensureDefaultTravel() {
+
+  if (!appState.travels) {
+    appState.travels = {};
+  }
+
+  if (!appState.activeTravelId) {
+    appState.activeTravelId = "travel_1";
+  }
+
   if (!appState.travels[appState.activeTravelId]) {
     appState.travels[appState.activeTravelId] = createEmptyTravel();
   }
+
 }
